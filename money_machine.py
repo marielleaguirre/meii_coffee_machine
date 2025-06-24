0b37ad7e52555bae2d869f70a124e0c96ec7b4b7
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-06-24 14:21:33 +0800

    Added the pseudocode for money machine

---
+++ money_machine.py
@@ -0,0 +1,31 @@
+# CLASS MoneyMachine:
+#     SET CURRENCY = "$"
+#     SET COIN_VALUES = dictionary of coin names and their values
+
+#     METHOD __init__():
+#         SET profit to 0
+#         SET money_received to 0
+
+#     METHOD report():
+#         PRINT current profit with currency symbol
+
+#     METHOD process_coins():
+#         PRINT accepted coin types
+#         FOR each coin type in COIN_VALUES:
+#             ASK user how many of that coin
+#             ADD total value of coins to money_received
+#         PRINT total money provided
+#         RETURN money_received
+
+#     METHOD make_payment(cost):
+#         CALL process_coins()
+#         IF money_received >= cost:
+#             CALCULATE change
+#             PRINT change returned
+#             ADD cost to profit
+#             RESET money_received to 0
+#             RETURN True
+#         ELSE:
+#             PRINT refund message
+#             RESET money_received to 0
+#             RETURN False
