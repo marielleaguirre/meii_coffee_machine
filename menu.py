fbead7cf1ac0c0905cf8b52520691d69852b8592
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-06-24 14:14:26 +0800

    Added a pseudocode for the menu

---
+++ menu.py
@@ -0,0 +1,21 @@
+# CLASS MenuItem:
+#     METHOD __init__(name, water, milk, coffee, cost):
+#         SET item name
+#         SET item cost
+#         SET ingredients as a dictionary of water, milk, coffee
+
+# CLASS Menu:
+#     METHOD __init__():
+#         CREATE a list of MenuItem objects: latte, espresso, cappuccino
+
+#     METHOD get_items():
+#         SET options to an empty string
+#         FOR each item in the menu:
+#             ADD item name and a slash to options
+#         RETURN options
+
+#     METHOD find_drink(order_name):
+#         FOR each item in the menu:
+#             IF item name matches order_name:
+#                 RETURN that item
+#         PRINT "Sorry that item is not available."
